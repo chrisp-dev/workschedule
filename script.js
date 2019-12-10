6f1bbfba2793f1151bed2abe7a3e7c3e59e7eb79
chrisp-dev <chris.paradeise@gmail.com>
2019-12-09 19:42:25 -0800

    Clean refactor, save animation is better

--- script.js
+++ script.js
@@ -1,83 +1,92 @@
 $(document).ready(function () {
     // step 1 - variables
     var hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-    var hourLabelArray = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
     var localContent = JSON.parse(localStorage.getItem('schedule')) || {};
-
-    // test localContent object:
-    // var lc = {
-    //     9: "Make breakfast",
-    //     10: "Begin Actual Work :)",
-    //     11: "Coffee break 15min",
-    //     12: "Lunch duh",
-    //     13: "Extended lunch",
-    //     14: "Second lunch",
-    //     15: "Bathroom...",
-    //     16: "Work a little",
-    //     17: "Ahhhh buh bye"
-    // }
-
-    // localStorage.setItem('schedule', JSON.stringify(lc));
 
     // step 2 - build HTML
     const container = $('.container');
 
     let currentDayDisplay = $("#currentDay");
     currentDayDisplay.text('Current Day: ' + moment().format('ll'));
-
-    // jq style function to allow chaining to style the textarea 
-    // based on the time of day
-    $.fn.styleHours = function (hour) {
-        let nowHour = moment().hour();
-
+    
+    /**
+     * styleHours
+     * @usage $('#area-'+hr).styleHours();
+     */
+    $.fn.styleHours = function () {
+        let nowHour = moment().hour() - 12;
+        let hour = $(this)[0].id.split("-")[1];
         if (nowHour === hour) {
-            // present
             $(this).addClass('present');
         } else if (nowHour > hour) {
             $(this).addClass('past');
         } else {
-            $(this).addclass('future');
+            $(this).addClass('future');
         }
         return this;
     }
-    // foreach hour build a row
-    hourArray.forEach(hour => {
-        let div = $('<div class=row>');
-        let timeBlock = $('<div class="hour col-12 col-sm-1 text-center">');
-        let timeBlockText = $('<span>');
-        timeBlockText.text(formatHour(hour));
-        timeBlock.append(timeBlockText);
+    
+    /**
+     * renderSchedule
+     * @description Accepts an array of Numbers and populates HTML
+     * @param {[Number]} hours 
+     */
+    function renderSchedule(hours) {
+        // foreach hour build a row
+        hours.forEach(hour => {
+            // CREATE row
+            let div = $('<div class=row>');
+            
+            // CREATE time
+            let timeBlock = $('<div class="hour col-12 col-sm-1 text-center">');
+            let timeBlockText = $('<span>');
+            timeBlockText.text(formatHour(hour));
+            timeBlock.append(timeBlockText);
+            
+            // CREATE text input
+            let txtArea = $('<textarea id="area-' + hour + '" class="col-10 col-sm-10">');
+            txtArea.styleHours();
+            if (localContent[hour] && localContent[hour].length) {
+                $(txtArea).val(localContent[hour]);
+            } else {
+                $(txtArea).val("");
+            }
+    
+            // CREATE save button
+            let saveBtn = $('<button id="save-' + hour + '" class="saveBtn col col-sm-1">');    
+            saveBtn.append($('<i class="fa fa-save">'));    
+            saveBtn.on('click', saveEvent);
+    
+            // APPEND time, txt, save to div
+            div.append(timeBlock, txtArea, saveBtn);
+            
+            // APPEND div to container
+            container.append(div);
+        });    
+    }
 
-        let txtArea = $('<textarea id="area-' + hour + '" class="col-10 col-sm-10">');
-        $(txtArea).styleHours(hour);
+    /**
+     * Saves the content of the current row to local storage
+     * @param evt 
+     */
+    function saveEvent(evt) {
+        evt.preventDefault();
+        let hr = this.id.split("-")[1];
+        let area = $(`#area-${hr}`);
 
-        if (localContent[hour] && localContent[hour].length) {
-            $(txtArea).val(localContent[hour]);
-        } else {
-            $(txtArea).val("");
-        }
+        // update the localContent object
+        localContent[hr] = area.val();
 
-        let saveBtn = $('<button id="save-' + hour + '" class="saveBtn col col-sm-1">');
+        // store the content in localStorage
+        localStorage.setItem("schedule", JSON.stringify(localContent));
 
-        saveBtn.append($('<i class="fa fa-save">'));
-
-        saveBtn.on('click', function (evt) {
-            evt.preventDefault();
-            let hr = this.id.split("-")[1];
-            let area = $(`#area-${hr}`);
-
-            area.addClass('blink');
-            localContent[hr] = area.val();
-            localStorage.setItem("schedule", JSON.stringify(localContent));
-            setTimeout(function () {
-                area.removeClass('blink');
-            }, 4000);
-        })
-
-        div.append(timeBlock, txtArea, saveBtn);
-
-        container.append(div);
-    });
+        // animate textarea for success indicator
+        area.addClass('blink');
+        setTimeout(function () {
+            // remove after 3s
+            area.removeClass('blink');
+        }, 3000);
+    }
 
     // utility fn to print AM or PM
     // use moment right? lol
@@ -86,4 +95,7 @@ $(document).ready(function () {
         if (hour > 12) return (hour - 12) + "PM";
         return hour + "AM";
     }
+
+    // render
+    renderSchedule(hourArray);
 })
